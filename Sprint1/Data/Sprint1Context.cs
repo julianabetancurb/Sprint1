903dc9ba5c1d13b4dde5d41efa8e224ed2657f00
juliana <julianabetancur320@gmail.com>
2023-09-30 16:41:49 -0500

    Agregar archivos de proyecto.

---
+++ Sprint1/Data/Sprint1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sprint1.Models;
+
+namespace Sprint1.Data
+{
+    public class Sprint1Context : DbContext
+    {
+        public Sprint1Context (DbContextOptions<Sprint1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sprint1.Models.Convocatoria> Convocatoria { get; set; } = default!;
+    }
+}
---
+++ Sprint1/Models/Convocatoria.cs
@@ -0,0 +1,13 @@
+﻿namespace Sprint1.Models
+{
+    public class Convocatoria
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Contenido { get; set; }
+        public string? Imagen { get; set; }
+        public string? NombreCreador { get;set; }
+
+
+    }
+}
